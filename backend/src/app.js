c632fcf3e3f3fae53e12586603eafe6c6d113d49
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 01:24:59 +0600

    func: success handler, error handler and router v1 add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
+import { successHandler } from "./middlewares/successHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import routerV1 from "./routers/v1.js";
 import express from "express";
 const app = express();
 
 app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(successHandler);
+
+// API routes
+app.use("/api/v1", routerV1);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Nodejs!");
