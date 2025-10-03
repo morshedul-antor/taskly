92354884a73ec4f994d776aa4d11db35c53ba1c7
morshedul-antor <antor.morshedul@gmail.com>
2025-10-03 21:59:44 +0600

    func: jwt token functions add

---
+++ backend/src/utils/token.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+
+export const createToken = async (id, time) => {
+  const token = jwt.sign({ sid: id }, config.jwtSecret, {
+    algorithm: config.algorithm,
+    expiresIn: time || "24h",
+  });
+
+  return { token: token, type: "bearer" };
+};
+
+export const validateToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret, {
+      algorithms: [config.algorithm],
+    });
+
+    const userId = decoded.sid;
+    return userId;
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      throw new Error("Token expired!");
+    } else if (error.name === "JsonWebTokenError") {
+      throw new Error("Invalid token!");
+    } else {
+      throw new Error("Token validation failed!");
+    }
+  }
+};
