ab3cdcb11b855d0d69995983f2c5bbcd2fc39435
morshedul-antor <antor.morshedul@gmail.com>
2025-10-03 21:48:50 +0600

    func: prisma and db connection add

---
+++ backend/src/config/database.js
@@ -0,0 +1,20 @@
+// src/config/database.js
+import { PrismaClient } from "../generated/prisma/index.js";
+
+export const prisma = new PrismaClient({
+  log: process.env.ENV === "dev" ? ["query", "error", "warn"] : ["error"],
+});
+
+export const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("✅ Database connected successfully.");
+  } catch (error) {
+    console.error("❌ Database connection error:", error);
+    process.exit(1);
+  }
+};
+
+export const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
