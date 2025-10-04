f64d734fc27686fd17219322c8ef3f05f5891091
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 20:39:49 +0600

    func: undefined routes function add

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,30 @@ import { successHandler } from "./middlewares/successHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import routerV1 from "./routers/v1.js";
 import express from "express";
+
 const app = express();
 
-app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(successHandler);
 
-// API routes
+// routes
 app.use("/api/v1", routerV1);
-app.use(errorHandler);
 
+// root api
 app.get("/", (req, res) => {
   res.send("Nodejs!");
 });
+
+// undefined routes
+app.use((req, res) => {
+  res.status(404).json({
+    status: "Failed",
+    message: `Route [${req.originalUrl}] not found!`,
+  });
+});
+
+// error handler
+app.use(errorHandler);
 
 export default app;
