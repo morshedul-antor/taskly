54ec95b4702c6ba3fc6bf6b83888a3f4876ea3d2
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 22:04:32 +0600

    feat: auth route add

--- backend/src/routers/v1.js
+++ backend/src/routers/v1.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import authRouter from "../api/auth.js";
 import userRouter from "../api/users.js";
 import taskRouter from "../api/tasks.js";
 
 const router = Router();
 
+router.use("", authRouter);
 router.use("/users", userRouter);
 router.use("/tasks", taskRouter);
 
