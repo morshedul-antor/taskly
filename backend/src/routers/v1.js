547142c9c146871e9696d105124737c2e6c8ca53
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 20:23:05 +0600

    feat: task routes add

--- backend/src/routers/v1.js
+++ backend/src/routers/v1.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "../api/users.js";
+import taskRouter from "../api/tasks.js";
 
 const router = Router();
 
 router.use("/users", userRouter);
+router.use("/tasks", taskRouter);
 
 export default router;
