b84356beda180d21f19025f6c93da69ae46354dc
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 20:22:46 +0600

    feat: task api endpoints add

---
+++ backend/src/api/tasks.js
@@ -0,0 +1,48 @@
+import taskService from "../services/tasks.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", async (req, res, next) => {
+  const skip = req.query.skip && Number(req.query.skip);
+  const limit = req.query.limit && Number(req.query.limit);
+
+  try {
+    const data = await taskService.get({ skip, limit });
+    res.success(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const data = await taskService.createTask(req.body);
+    res.created(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/tasks-with-user/", async (req, res, next) => {
+  const skip = req.query.skip && Number(req.query.skip);
+  const limit = req.query.limit && Number(req.query.limit);
+
+  try {
+    const data = await taskService.tasksWithUser(skip, limit);
+    res.success(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const data = await taskService.getById(Number(req.params.id));
+    res.success(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
