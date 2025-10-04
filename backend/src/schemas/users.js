95878fe18e96e4c86067391091081579577a4c9d
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 20:19:27 +0600

    func: user schema add

---
+++ backend/src/schemas/users.js
@@ -0,0 +1,13 @@
+export const userOut = {
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  name: true,
+  email: true,
+};
+
+export const userInfoOut = {
+  id: true,
+  name: true,
+  email: true,
+};
