4697f3b8489efd3e9c648610fb20dc85971c7445
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 21:32:46 +0600

    fix: import issue

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import config from "../config/config";
+import config from "../config/config.js";
 
 export const createToken = async (id, time) => {
   const token = jwt.sign({ sid: id }, config.jwtSecret, {
