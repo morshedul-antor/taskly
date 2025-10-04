3817c4204014f7e94b4a3e9f650ed03f4e5ae862
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 20:21:36 +0600

    modify: user api endpoints

--- backend/src/api/users.js
+++ backend/src/api/users.js
@@ -1,3 +1,4 @@
+import { userOut } from "../schemas/users.js";
 import userService from "../services/users.js";
 import { Router } from "express";
 
@@ -5,7 +6,7 @@ const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const data = await userService.getUsers();
+    const data = await userService.get({ select: userOut });
     res.success(data);
   } catch (error) {
     next(error);
@@ -23,7 +24,18 @@ router.post("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   try {
-    const data = await userService.getUserById(Number(req.params.id));
+    const data = await userService.getById(Number(req.params.id), {
+      select: userOut,
+    });
+    res.success(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/user-by-id-with-tasks/:id", async (req, res, next) => {
+  try {
+    const data = await userService.getByIdWithTasks(Number(req.params.id));
     res.success(data);
   } catch (error) {
     next(error);
