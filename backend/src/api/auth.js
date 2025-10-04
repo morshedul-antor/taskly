bbd6fbaa94b820fbc2ee02e3594e527876e39e00
morshedul-antor <antor.morshedul@gmail.com>
2025-10-04 22:03:45 +0600

    feat: login and auth api endpoints add

---
+++ backend/src/api/auth.js
@@ -0,0 +1,26 @@
+import { isAuth } from "../middlewares/isAuth.js";
+import { userOut } from "../schemas/users.js";
+import userService from "../services/users.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const data = await userService.login(req.body);
+    res.created(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/auth", isAuth, async (req, res, next) => {
+  try {
+    const data = await userService.getById(req.user.id, { select: userOut });
+    res.created(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
