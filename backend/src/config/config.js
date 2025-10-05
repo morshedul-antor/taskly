480af0c936bae026de143ebe69104443ba743811
morshedul-antor <antor.morshedul@gmail.com>
2025-10-05 12:31:53 +0600

    modify: port number

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 8000,
   databaseUrl: process.env.DATABASE_URL,
   env: process.env.ENV || "dev",
 
