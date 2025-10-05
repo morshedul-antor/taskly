52892031fc85c4af9096901e754a5ebd0449e313
morshedul-antor <antor.morshedul@gmail.com>
2025-10-05 11:27:03 +0600

    add: searchbar molecule component

---
+++ frontend/src/components/molecules/SearchBar.jsx
@@ -0,0 +1,21 @@
+import Input from '../atoms/Input'
+import { Search } from 'lucide-react'
+
+const SearchBar = ({ value, onChange, placeholder = 'Search...', className = '' }) => {
+  return (
+    <div className={`relative ${className}`}>
+      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+        <Search className="h-5 w-5 text-gray-400" />
+      </div>
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="pl-10 w-full"
+      />
+    </div>
+  )
+}
+
+export default SearchBar
