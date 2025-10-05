7090467e75c1484a3706d39d2c1cbeb484eca702
morshedul-antor <antor.morshedul@gmail.com>
2025-10-05 11:24:58 +0600

    add: select atom component

---
+++ frontend/src/components/atoms/Select.jsx
@@ -0,0 +1,20 @@
+const Select = ({ options, error, className = '', ...props }) => {
+  const baseClasses =
+    'block w-full px-4 py-3 rounded-xl border bg-white/90 backdrop-blur-sm transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-1'
+
+  const normalClasses = 'border-light-grey focus:border-primary focus:ring-primary/20'
+
+  const errorClasses = 'border-red focus:border-red focus:ring-red/20'
+
+  return (
+    <select className={`${baseClasses} ${error ? errorClasses : normalClasses} ${className}`} {...props}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+export default Select
